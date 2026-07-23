806634b816c38a956be5c4d3b68ba9e69e44e59f
Ashish-Panwar <ap.ashish1306@gmail.com>
2026-07-23 13:03:52 +0000

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
